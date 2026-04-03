454dcc1f90dcfc43a89fb5847306ce46627ec5f2
Robbe Van Mele <robbe.vanmele@student.kdg.be>
2026-04-03 09:15:27 +0200

    Fix: #7 removed home page.

--- cookbook-frontend/src/app/app.routes.ts
+++ cookbook-frontend/src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    loadComponent: () => import('@features/home/home.page')
-  },
+
   {
     path: 'recipes',
     loadComponent: () => import('@features/recipe/recipe.page')
   },
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'recipes'
   }
 ];
