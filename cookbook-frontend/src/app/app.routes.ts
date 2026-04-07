deae18330438a80f2e3fddade6245c7c06ca6313
Robbe Van Mele <robbe.vanmele@student.kdg.be>
2026-04-07 15:48:01 +0200

    Feat: #8 implemented login page + auth functionality

--- cookbook-frontend/src/app/app.routes.ts
+++ cookbook-frontend/src/app/app.routes.ts
@@ -1,13 +1,23 @@
 import { Routes } from '@angular/router';
+import {authGuard} from '@core/services/auth/auth.guard';
 
 export const routes: Routes = [
-
+  {
+    path: 'login',
+    loadComponent: () => import('@core/components/typescript/auth/login.page'),
+  },
+  {
+    path: 'auth/callback',
+    loadComponent: () =>
+      import('@core/components/typescript/auth/callback.component').then((m) => m.CallbackComponent),
+  },
   {
     path: 'recipes',
-    loadComponent: () => import('@features/recipe/recipe.page')
+    canActivate: [authGuard],
+    loadComponent: () => import('@features/recipe/recipe.page'),
   },
   {
     path: '**',
-    redirectTo: 'recipes'
-  }
+    redirectTo: 'recipes',
+  },
 ];
