a7a50dfaf38e9c4e578d35ff981e166319d39b58
Robbe Van Mele <robbe.vanmele@student.kdg.be>
2026-04-08 11:19:22 +0200

    Feat: #8 enhanced interceptor + added remember me

--- cookbook-frontend/src/app/core/services/auth/auth.interceptor.ts
+++ cookbook-frontend/src/app/core/services/auth/auth.interceptor.ts
@@ -1,9 +1,51 @@
-import {HttpInterceptorFn} from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '@core/services/auth/auth.service';
+import { catchError, switchMap, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('jwt');
-  if (token) {
-    req = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
+  const authService = inject(AuthService);
+  const accessToken = authService.getAccessToken();
+
+  const isRefreshRequest = req.url.includes('/auth/refresh');
+
+  if (accessToken && !isRefreshRequest) {
+    req = req.clone({ setHeaders: { Authorization: `Bearer ${accessToken}` } });
   }
-  return next(req);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+
+      if ((error.status === 401 || error.status === 403) && !isRefreshRequest) {
+
+        const refreshToken = authService.getRefreshToken();
+
+        if (!refreshToken) {
+          authService.logout();
+          return throwError(() => error);
+        }
+
+        // attempt to refresh
+        return authService.refreshSession().pipe(
+          switchMap((res) => {
+            const clonedReq = req.clone({
+              setHeaders: { Authorization: `Bearer ${res.accessToken}` }
+            });
+            return next(clonedReq);
+          }),
+          catchError((refreshError) => {
+            authService.logout();
+            return throwError(() => refreshError);
+          })
+        );
+      }
+
+      // If refresh request fails, logout
+      if ((error.status === 401 || error.status === 403) && isRefreshRequest) {
+        authService.logout();
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
