475ac0b8d5c346d491319ef851b69a340d3248de
Robbe Van Mele <robbe.vanmele@student.kdg.be>
2026-05-04 12:40:22 +0200

    Feat: #12 added cooking mode

--- cookbook-frontend/src/app/features/recipe/recipe-detail.page.ts
+++ cookbook-frontend/src/app/features/recipe/recipe-detail.page.ts
@@ -1,16 +1,19 @@
-import { Component, inject, input } from '@angular/core';
-import { RecipeService } from '@shared/services/recipe';
-import { RecipeDto } from '@shared/domain/recipe';
-import { rxResource } from '@angular/core/rxjs-interop';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
-import { MatChip, MatChipSet } from '@angular/material/chips';
-import { MatIcon } from '@angular/material/icon';
-import { MatDivider } from '@angular/material/list';
-import { RecipeIngredientsComponent } from '@features/recipe/components/typescript/recipe-ingredients-list.component';
-import { RecipePreparationComponent } from '@features/recipe/components/typescript/recipe-preparation-list';
+import {ChangeDetectionStrategy, Component, inject, input, signal} from '@angular/core';
+import {RecipeService} from '@shared/services/recipe';
+import {RecipeDto} from '@shared/domain/recipe';
+import {rxResource} from '@angular/core/rxjs-interop';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {MatChip, MatChipSet} from '@angular/material/chips';
+import {MatIcon} from '@angular/material/icon';
+import {MatDivider} from '@angular/material/list';
+import {MatButton} from '@angular/material/button';
+import {RecipeIngredientsComponent} from '@features/recipe/components/typescript/recipe-ingredients-list.component';
+import {RecipePreparationComponent} from '@features/recipe/components/typescript/recipe-preparation-list';
+import {RecipeCookingModeComponent} from '@features/recipe/components/typescript/recipe-cooking-mode.component';
 
 @Component({
   selector: 'app-recipe-detail-page',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './recipe-detail.page.html',
   imports: [
     MatProgressSpinner,
@@ -18,8 +21,10 @@ import { RecipePreparationComponent } from '@features/recipe/components/typescri
     MatChip,
     MatIcon,
     MatDivider,
+    MatButton,
     RecipeIngredientsComponent,
-    RecipePreparationComponent
+    RecipePreparationComponent,
+    RecipeCookingModeComponent
   ],
   styleUrls: ['./recipe-detail.page.scss']
 })
@@ -27,9 +32,18 @@ export default class RecipeDetailPage {
   readonly recipeService = inject(RecipeService);
 
   readonly recipeId = input.required<string>();
+  readonly isCookingMode = signal(false);
 
   readonly recipe = rxResource<RecipeDto, string | undefined>({
     params: () => this.recipeId(),
     stream: ({ params }) => this.recipeService.getRecipeById(params)
   });
+
+  enterCookingMode(): void {
+    this.isCookingMode.set(true);
+  }
+
+  exitCookingMode(): void {
+    this.isCookingMode.set(false);
+  }
 }
