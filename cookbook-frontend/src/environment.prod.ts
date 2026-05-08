89e5b743691e1fd974d341e6892259754aa3c136
Robbe Van Mele <robbe.vanmele@student.kdg.be>
2026-05-08 13:34:51 +0200

    Fix: #42 fix url prod

--- cookbook-frontend/src/environment.prod.ts
+++ cookbook-frontend/src/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://d64m1ytsn90cc.cloudfront.net/api',
-  authRedirectUrl: 'https://d64m1ytsn90cc.cloudfront.net/'
+  apiUrl: '/api',
+  authRedirectUrl: 'https://d64m1ytsn90cc.cloudfront.net'
 };
