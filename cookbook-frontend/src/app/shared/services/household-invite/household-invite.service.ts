26a81e510af5376e93ea17ec79bdad460fcc542e
Robbe Van Mele <robbe.vanmele@student.kdg.be>
2026-05-20 12:39:43 +0200

    Feat #81: implemented bugfix 81

--- cookbook-frontend/src/app/shared/services/household-invite/household-invite.service.ts
+++ cookbook-frontend/src/app/shared/services/household-invite/household-invite.service.ts
@@ -1,15 +1,16 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {
-  CreateHouseholdInviteRequest,
-  HouseholdInvite,
-} from '@shared/domain/household-invite';
+import { CreateHouseholdInviteRequest, HouseholdInvite, HouseholdInviteDto, } from '@shared/domain/household-invite';
 
 @Injectable({ providedIn: 'root' })
 export class HouseholdInviteService {
   private readonly http = inject(HttpClient);
   private readonly apiUrl = '/api/household-invites';
+
+  getInvite(inviteId: string): Observable<HouseholdInviteDto> {
+    return this.http.get<HouseholdInviteDto>(`${this.apiUrl}/${inviteId}`);
+  }
 
   createInvite(
     householdId: string,
